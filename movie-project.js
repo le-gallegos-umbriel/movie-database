1a2a54cbacbdad35200cfa87e8b8b7d543eee121
Cynthia Gallegos <cynthialgallegos97@gmail.com>
2022-06-07 12:27:39 -0500

    complete-nostyling

--- movie-project.js
+++ movie-project.js
@@ -3,22 +3,72 @@ $(document).ready(function () {
 
 
 const URL = 'https://innate-dent-tadpole.glitch.me/movies';
-fetch(URL).then(res => {
-    document.getElementById("movieDisplay").innerHTML = ("Loading...");
-    return res.json()})
-    .then(movies => { document.getElementById("movieDisplay").innerHTML = ("");
-    console.log(movies);
-    movies.map((movie) => {
-        document.getElementById("movieDisplay")
-            .innerHTML += enterMovie(movie);
+function refreshMovies () {
+    fetch(URL).then(res => {
+        document.getElementById("movieDisplay").innerHTML = ("Loading...");
+        return res.json()
     })
-});
+        .then(movies => {
+            document.getElementById("movieDisplay").innerHTML = ("");
+            console.log(movies);
+            movies.map((movie) => {
+                document.getElementById("movieDisplay")
+                    .innerHTML += enterMovie(movie);
+            })
+        }).then(result => {
+        $('.delete').each(function () {
+            $(this).click(function () {
+                const movieNumber = $(this).attr("data-id");
+                console.log(parseInt(movieNumber));
+                deleteMovie(movieNumber);
+
+            });
+        });
+        $('.edit').each(function () {
+            $(this).click(function () {
+                const movieNumber = $(this).attr("data-id");
+                editMovie(movieNumber);
+                // const movieNumber = $(this).attr("data-id");
+                // let title;
+                // let rating;
+                // let genre;
+                // if ($('#title').val() === "") {
+                //     title = "N/A";
+                // } else {
+                //     title = $('#title').val();
+                // }
+                // if ($('#rating').val() === "") {
+                //     rating = "N/A";
+                // } else {
+                //     rating = $('#rating').val();
+                // }
+                // if ($('#genre').val() === "") {
+                //     genre = "N/A";
+                // } else {
+                //     genre = $('#genre').val();
+                // }
+                // const newMovieObj =
+                //     {
+                //         title: title,
+                //         rating: rating,
+                //         genre: genre
+                //     }
+                // console.log(parseInt(movieNumber));
+                // editMovie(newMovieObj, movieNumber);
+
+            });
+
+        });
+
+    });
+}
+    refreshMovies();
 
 //write a function that displays the title and rating on HTML
 
-function enterMovie (movieName){
-    let html = "";
-    html+= `<div>
+    function enterMovie(movieName) {
+        let html = "";
+        html += `<div>
   <div class="card-header">
      <h1>Title: ${movieName.title}</h1>
   </div>
@@ -29,14 +79,89 @@ function enterMovie (movieName){
     <li class="list-group-item">
     <h3>ID: ${movieName.id}</h3>
     </li>
+        <li class="list-group-item">
+    <h3>Genre: ${movieName.genre}</h3>
+    </li>
+   
     </ul>
+    <button class="delete" data-id="${movieName.id}"> delete </button>
+    <button class="edit" data-id="${movieName.id}"> edit </button>
 </div>`;
-    return html;
-}
+        return html;
+    }
+
+//// edit movie ////
+    function editMovie(num) {
+        // fetch(`${URL}/${num}`).then(res => res.json());
+        const url = `${URL}/${num}`;
+        console.log(url)
+        const options = {
+            method: 'GET'
+        };
+        fetch(url, options)
+            .then(res => {
+                console.log(res);
+                return res.json();
+            }).then(movie => {
+            let title;
+            let rating;
+            let genre;
+            if (movie.title === ""  || movie.title === undefined) {
+                title = "N/A";
+            } else {
+                title = movie.title;
+            }
+            if (movie.rating === "" || movie.rating === undefined) {
+                rating = "N/A";
+            } else {
+                rating = movie.rating;
+            }
+            if (movie.genre === "" || movie.genre === undefined) {
+                genre = "N/A";
+            } else {
+                genre = movie.genre;
+            }
+            $("#title").val(title);
+            $("#rating").val(rating);
+            $("#genre").val(genre);
+            $("#id").val(movie.id);
+        })
+            .catch(/* handle errors */);
+    }
+// editMovie(274);
+
+//// delete movie ////
+
+    function deleteMovie(num) {
+        const url = `${URL}/${num}`;
+        console.log(url)
+        const options = {
+            method: 'DELETE'
+        };
+        fetch(url, options)
+            .then(console.log("You're movie was successfully deleted!"))
+            .then(refreshMovies)
+            .catch(/* handle errors */);
+    }
+
+    ////
+    function editSubmit(movieObj, num) {
+        const url = `${URL}/${num}`;
+        const options = {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(movieObj),
+        };
+        fetch(url, options)
+            .then(console.log("You're movie was successfully edited!"))
+            .catch(/* handle errors */);
+    }
 
 /// enter movie to URL ///
-
-    function postMovie (movieObj) {
+///edit submit button /////
+    function postMovie(movieObj) {
         const url = URL;
         const options = {
             method: 'POST',
@@ -50,7 +175,8 @@ function enterMovie (movieName){
             .catch(/* handle errors */);
 
     }
-   //////
+
+    //////
 
     $('#submit').click(function (e) {
         e.preventDefault();
@@ -76,15 +202,66 @@ function enterMovie (movieName){
             genre = $('#genre').val();
         }
         const newMovieObj =
-            { title: title,
+            {
+                title: title,
                 rating: rating,
                 genre: genre
             }
-            console.log(newMovieObj);
+        console.log(newMovieObj);
         postMovie(newMovieObj);
+    });
+
+    //submit edit
+    $('#editrightside').click(function (e) {
+        e.preventDefault();
+        console.log($('#title').val());
+        console.log($('#rating').val());
+        console.log($('#genre').val());
+        let title;
+        let rating;
+        let genre;
+        let id;
+        if ($('#title').val() === "") {
+            title = "N/A";
+        } else {
+            title = $('#title').val();
+        }
+        if ($('#rating').val() === "") {
+            rating = "N/A";
+        } else {
+            rating = $('#rating').val();
+        }
+        if ($('#genre').val() === "") {
+            genre = "N/A";
+        } else {
+            genre = $('#genre').val();
+        }
+        const newMovieObj =
+            {
+                title: title,
+                rating: rating,
+                genre: genre
+            }
+        console.log(newMovieObj);
+        editNewMovie(newMovieObj,$('#id').val());
     })
 
+    ///////////
+    function editNewMovie(movieObj, num) {
+        const url = `${URL}/${num}`;
+        const options = {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(movieObj),
+        };
+        fetch(url, options)
+            .then(console.log("Your edit has been processed."))
+            .then (res => refreshMovies())
+            .catch(/* handle errors */);
 
+    }
 
 ////// end of jquery
 });
