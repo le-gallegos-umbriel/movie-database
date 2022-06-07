f48c7fe18e56da1330f07d91778ce86acdf80653
Cynthia Gallegos <cynthialgallegos97@gmail.com>
2022-06-07 09:52:05 -0500

    ovie

--- movie-project.js
+++ movie-project.js
@@ -19,12 +19,71 @@ fetch(URL).then(res => {
 function enterMovie (movieName){
     let html = "";
     html+= `<div>
-    <h1>Title: ${movieName.title}</h1>
+  <div class="card-header">
+     <h1>Title: ${movieName.title}</h1>
+  </div>
+    <ul class="list-group list-group-flush">
+    <li class="list-group-item">
     <h3> Rating: ${movieName.rating}</h3>
+    </li>
+    <li class="list-group-item">
     <h3>ID: ${movieName.id}</h3>
+    </li>
+    </ul>
 </div>`;
     return html;
 }
+
+/// enter movie to URL ///
+
+    function postMovie (movieObj) {
+        const url = URL;
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(movieObj),
+        };
+        fetch(url, options)
+            .then(console.log("You're movie was successfully entered!"))
+            .catch(/* handle errors */);
+
+    }
+   //////
+
+    $('#submit').click(function (e) {
+        e.preventDefault();
+        console.log($('#title').val());
+        console.log($('#rating').val());
+        console.log($('#genre').val());
+        let title;
+        let rating;
+        let genre;
+        if ($('#title').val() === "") {
+            title = "N/A";
+        } else {
+            title = $('#title').val();
+        }
+        if ($('#rating').val() === "") {
+            rating = "N/A";
+        } else {
+            rating = $('#rating').val();
+        }
+        if ($('#genre').val() === "") {
+            genre = "N/A";
+        } else {
+            genre = $('#genre').val();
+        }
+        const newMovieObj =
+            { title: title,
+                rating: rating,
+                genre: genre
+            }
+            console.log(newMovieObj);
+        postMovie(newMovieObj);
+    })
+
 
 
 ////// end of jquery
